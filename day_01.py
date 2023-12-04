c07ed030213b64d7cdd750424f75e51075ea7b66
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2023-12-04 12:12:00 +0930

    Update day_01.py

--- day_01.py
+++ day_01.py
@@ -2,22 +2,26 @@ import re
 
 with open('day') as f: txt = f.read()
 
-lines = txt.splitlines()
+input_lines = txt.splitlines()
 
 # Extract the two digit number from the string
-def get_num(line, numbers):
+def get_num(lines, numbers):
     re_num = f"{'|'.join(numbers)}"
+    total = 0
     
-    re_start = re.findall(f"^.*?({re_num})", line)[0]
-    re_start = str(numbers.index(re_start)) if re_start in numbers else re_start
+    for line in lines:
+        re_start = re.findall(f"^.*?({re_num})", line)
+        re_start = '' if not len(re_start) else str(numbers.index(re_start[0])) if re_start[0] in numbers else re_start[0]
+        
+        re_end = re.findall(f"^.*({re_num})", line)
+        re_end = '0' if not len(re_end) else str(numbers.index(re_end[0])) if re_end[0] and re_end[0] in numbers else re_end[0]
+        
+        total += int(re_start + re_end)
     
-    re_end = re.findall(f"^.*({re_num})(?!{re_num}).*?$", line)[0]
-    re_end = str(numbers.index(re_end)) if re_end in numbers else re_end
-    
-    return (int(re_start + re_end))
+    return (total)
 
 # Part 1
-print(sum([get_num(line, ['\d']) for line in lines]))
+print(get_num(input_lines, ['\d']))
 
 # Part 2
-print(sum([get_num(line, ['\d','one','two','three','four','five','six','seven','eight','nine']) for line in lines]))
+print(get_num(input_lines, ['\d','one','two','three','four','five','six','seven','eight','nine']))
