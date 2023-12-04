3b74e6ea2750103e66d4a1d5ced216d42c2ad7bf
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2023-12-04 20:53:56 +0930

    Update day_04.py

--- day_04.py
+++ day_04.py
@@ -1,12 +1,13 @@
-import numpy
+from collections import deque
 
 with open('day') as f: txt = f.read()
 
 lines = txt.splitlines()
+cards = deque()
 total = 0
 
-for line in lines:
-    nums = line.split(': ')[1].split(' | ')
+def process_scratchcard(scratch):
+    nums = scratch.split(': ')[1].split(' | ')
     win_nums = nums[0].split(' ')
     scratch_nums = nums[1].split(' ')
     wins = 0
@@ -15,7 +16,19 @@ for line in lines:
         if win_num and win_num in scratch_nums:
             wins += 1
     
-    if wins:
-        total += pow(2, wins - 1)
+    return wins
 
-print(total)
+# Part 1
+results = [process_scratchcard(line) for line in lines]
+print(sum([pow(2, result - 1) if result else 0 for result in results]))
+
+# Part 2
+cards = [[0, card] for card in lines[::-1]]
+for index, card in enumerate(cards):
+    card_wins = process_scratchcard(card[1])
+    
+    if card_wins:
+        card_wins += sum([new_card[0] for new_card in cards[index - card_wins : index]])
+    card[0] = card_wins
+
+print(sum([card[0] for card in cards]) + len(cards))
