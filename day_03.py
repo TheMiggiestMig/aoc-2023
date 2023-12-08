91a3f2e8d1f1778e1e5f9b3624d93dab620a91e1
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2023-12-08 20:43:10 +0930

    Refactored code and commented more.

--- day_03.py
+++ day_03.py
@@ -1,44 +1,54 @@
 import numpy
 
 with open('day') as f: txt = f.read()
-
 lines = txt.splitlines()
 
-total = 0
-
-# Parse and flag the elements
-num_string = ''
-adjacent = None
+total = 0   # For Part 1
+part_number = ''
+adjacent_symbols = {}
 gears = {}
         
 for row, line in enumerate(lines):
     for column, element in enumerate(line):
         
-        # Check only if a number
-        if element in '0123456789':
-            num_string += element
+        # We'll extract the numbers on each line, and base everything from them.
+        if element.isdigit():
+            part_number += element
             
-            # Check the cardinal directions
-            top_left = (row - 1 if row else row, column - 1 if column else column)
-            bottom_right = (row + 2 if row < len(lines) - 1 else row, column + 2 if column < len(line) - 1 else column)
+            # Check the neighboring squares for symbols, keeping within the bounds.
+            bounds = {
+                'top': row - 1 if row else row,
+                'left': column - 1 if column else column,
+                'bottom': row + 2 if row < len(lines) - 1 else row,
+                'right':column + 2 if column < len(line) - 1 else column
+            }
             
-            for y in range(top_left[0], bottom_right[0]):
-                for x in range(top_left[1], bottom_right[1]):
-                    if lines[y][x] not in '0123456789.':
-                        adjacent = (lines[y][x], x, y)
-                        
-        elif num_string:
-            if adjacent:
-                total += int(num_string)
+            for y in range(bounds['top'], bounds['bottom']):
+                for x in range(bounds['left'], bounds['right']):
+                    
+                    if lines[y][x] not in '0123456789.':    # Not a number or '.', so a symbol.
+                        adjacent_symbols[(x, y)] = lines[y][x]
+        
+        # If it's not a number and we have previously detected numbers, then we've reached the end of the number.
+        # Check if it's a part number by seeing if it was adjacent to any symbols.
+        elif part_number:
+            if len(adjacent_symbols):
                 
-                if adjacent[0] == '*':
+                # Add the part number to the total (Part 1)
+                total += int(part_number)
+                
+                for symbol_coords in adjacent_symbols:
+                    symbol = adjacent_symbols[symbol_coords]
+                    if symbol == '*':
+                        
+                        # Use the gear's position as a key and add the value to the list of values (or create a new list if none exists)
+                        if not gears.get(symbol_coords):
+                            gears[symbol_coords] = []
+                            
+                        gears[symbol_coords] += [int(part_number)]
                     
-                    # This can really be done better :/
-                    # Use the gear's position as a key and add the value to the list of values (or create a new list if none exists)
-                    gears[f"x{adjacent[1]}y{adjacent[2]}"] = gears[f"x{adjacent[1]}y{adjacent[2]}"] + [int(num_string)] if gears.get(f"x{adjacent[1]}y{adjacent[2]}") else [int(num_string)]
-                    
-            num_string = ''
-            adjacent = None
+            part_number = ''
+            adjacent_symbols = {}
 
 # Part 1
 print(total)
