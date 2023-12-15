a1f042e9949c82c5736fb844f5f0dcc81c84c54d
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2023-12-15 11:53:58 +0930

    Update day_10.py (part 2)

--- day_10.py
+++ day_10.py
@@ -3,19 +3,10 @@ from collections import deque
 
 map_grid = [line.strip() for line in open("day").read().splitlines()]
 
-# WARNING: I wrote this solution this way because I hate everyone (inclusive) after 4hrs sleep or less.
+# WARNING: I wrote this solution this way because after 4hrs sleep or less, I hate everyone (inclusive).
 
 # The keys for the pipe directions
-'''
-| is a vertical pipe connecting north and south.
-- is a horizontal pipe connecting east and west.
-L is a 90-degree bend connecting north and east.
-J is a 90-degree bend connecting north and west.
-7 is a 90-degree bend connecting south and west.
-F is a 90-degree bend connecting south and east.
-. is ground; there is no pipe in this tile.
-S is the starting position of the animal; there is a pipe on this 
-'''
+#
 #   NESW
 # 0b1111
 
@@ -36,6 +27,16 @@ pipe_bit_mask = {
 }
 
 
+# Helper functions
+def left_normal(direction):
+    return direction << 1 & 0xf or 1
+
+def right_normal(direction):
+    return direction >> 1 or 0b1000
+
+def reverse(direction):
+    return direction ^ (0b1010 if direction & 0b1010 else 0b0101)
+
 # Find the starting position
 def find_start(map_grid):
     for y, row in enumerate(map_grid):
@@ -49,15 +50,9 @@ start_coords = find_start(map_grid)
 # Find one of the starting directions.
 current_direction = 0
 current_coord = (0, 0)
-moves = [
-    (-1, 0, 0b0100),    # Move East
-    (0, -1, 0b0010),    # Move South
-    (1, 0, 0b0001),     # Move West
-    (0, 1, 0b1000)      # Move North
-    ]
 
-for move in moves:
-    map_grid_point = map_grid[start_coords[1] + move[1]][start_coords[0] + move[0]]
+for direction_mask, (offset_x, offset_y) in direction_bit_mask.items():
+    map_grid_point = map_grid[start_coords[1] + offset_y][start_coords[0] + offset_x]
     
     # If it is a '.', we don't care.
     if map_grid_point == '.':
@@ -65,25 +60,34 @@ for move in moves:
     
     pipe = pipe_bit_mask[map_grid_point]
     
-    direction = move[2]
+    direction = reverse(direction_mask)
     
     # Check if the grid point we're looking at points back to 'S'.
     if pipe & direction:
         current_direction = direction
-        current_coord = (start_coords[0] + move[0], start_coords[1] + move[1])
+        current_coord = (start_coords[0] + offset_x, start_coords[1] + offset_y)
         break
 
 # Step through the pipes until we reach 'S' again.
 # Track the farthest point while we're at it.
 steps = 1
 current_pipe = map_grid[current_coord[1]][current_coord[0]]
-farthest_point = (1, steps)
 
-path_coords = deque()   # Track the coords that are part of our path for Part 2
-path_coords.append(current_coord)
+left_turns = 0
+right_turns = 0
+
+path_coords = deque([current_coord])   # Track the coords that are part of our path for Part 2
+path_directions = deque([current_direction])
 
 while current_pipe != 'S':
     pipe_mask = pipe_bit_mask[current_pipe]
+    
+    # Check if we're turning and add to the counters.
+    if (pipe_mask ^ 0b1010) and (pipe_mask ^ 0b0101):
+        if left_normal(reverse(current_direction)) & (pipe_mask ^ current_direction):
+            left_turns += 1
+        else:
+            right_turns += 1
     
     # Move us to the next pipe.
     # Do this by flipping the bit (turning off) the direction we came from.
@@ -98,18 +102,51 @@ while current_pipe != 'S':
     current_coord = (current_coord[0] + move_coord[0], current_coord[1] + move_coord[1])
     current_pipe = map_grid[current_coord[1]][current_coord[0]]
     path_coords.append(current_coord)
+    path_directions.append(current_direction)
     
     steps += 1
-    
-    # Now track how far we are from the 'S'. Use good ol' Manhattan Distance.
-    distance = math.sqrt((abs(current_coord[0] - start_coords[0])**2) + (abs(current_coord[1] - start_coords[1])**2))
-    if distance > farthest_point[0]:
-        farthest_point = (distance, steps)
 
 # Part 1
 print(steps//2)
 
-
+########################################
 # Part 2 (wtf)
-print(path_coords)
+checked_coords = deque()
+confirmed_coords = set()
+check_normal = left_normal if left_turns > right_turns else right_normal
+
+# Helper functions
+def scan_normals(coord, normal, checked):
+    offset_x, offset_y = direction_bit_mask[normal]
     
+    target_coord = (coord[0] + offset_x, coord[1] + offset_y)
+    while (not target_coord in path_coords) and (not target_coord in checked_coords):
+        checked_coords.append(target_coord)
+        target_coord = (target_coord[0] + offset_x, target_coord[1] + offset_y)
+
+# Start by replacing the 'S' position with a valid pipe
+pipe_mask = reverse(path_directions[0]) ^ path_directions[-1]
+pipe = [pipe for pipe, bit_mask in pipe_bit_mask.items() if bit_mask == pipe_mask][0]
+map_grid[start_coords[1]] = map_grid[start_coords[1]].replace('S', pipe)
+
+# Scan each entering normal and exiting normal for the path coords.
+for coord, direction in list(zip(path_coords, path_directions)):
+    enter_normal = check_normal(reverse(direction))
+    exit_normal = check_normal(direction ^ pipe_bit_mask[map_grid[coord[1]][coord[0]]])
+    
+    scan_normals(coord, enter_normal, checked_coords)
+    scan_normals(coord, exit_normal, checked_coords)
+
+# Go through each checked coord, confirm them and check the neighboring coords.
+target_inner_coord = None
+while len(checked_coords):
+    target_inner_coord = checked_coords.popleft()
+    confirmed_coords.add(target_inner_coord)
+    
+    for (offset_x, offset_y) in direction_bit_mask.values():
+        target_coord = (target_inner_coord[0] + offset_x, target_inner_coord[1] + offset_y)
+        if (not target_coord in path_coords) and (not target_coord in checked_coords) and (not target_coord in confirmed_coords):
+            checked_coords.append(target_coord)
+    
+
+print(len(confirmed_coords))
