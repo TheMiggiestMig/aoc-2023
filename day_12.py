5c596e9f3f9360e205b0226e330e297c35a9e517
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2023-12-18 21:11:10 +0930

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -6,35 +6,37 @@ lines = [line.strip() for line in open("day").read().splitlines()]
 rows = []
 for line in lines:
     arrangement, numbers = line.split(' ')
+    numbers = tuple([int(number) for number in numbers.split(',')])
     rows.append((arrangement, numbers))
 
 
 @lru_cache(maxsize=None)
 def scan_line(arrangement, numbers):
     if not len(numbers):
-        if arrangement.find('#')+1:
+        if '#' in arrangement:
             return 0
         else:
             return 1
     if arrangement[0] == '#': return 0
-    numbers = list([int(number) for number in numbers.split(',')])
+    
     number = numbers[0]
-    next_numbers = numbers[1:]
+    next_numbers = tuple(numbers[1:])
     space = len(arrangement) - (sum(numbers) + len(next_numbers))
     
     valid = 0
     
     for sp in range(space):
-        test_str = (sp+1)*'.'+number*'#'
+        test_str = (sp + 1) * '.' + number*'#'
         
-        test = all([a in [t,'?'] for a, t in zip(arrangement[:number+sp+1], test_str)])
+        test = all([a in [t,'?'] for a, t in zip(arrangement[:number + sp + 1], test_str)])
         if test:
-            valid += scan_line(arrangement[number+sp+1:], ','.join([str(num) for num in next_numbers]))
+            valid += scan_line(arrangement[number + sp + 1:], next_numbers)
     return valid
 
 arrangement, numbers = rows[4]
 
+# Part 1
+print(sum([scan_line('.' + arrangement, numbers) for arrangement, numbers in rows]))
 
-print(sum([scan_line('.'+arrangement, numbers) for arrangement, numbers in rows]))
-
-print(sum([scan_line('.'+'?'.join([arrangement]*5), ','.join([numbers]*5)) for arrangement, numbers in rows]))
+# Part 2
+print(sum([scan_line('.' + '?'.join([arrangement] * 5), tuple(numbers * 5)) for arrangement, numbers in rows]))
